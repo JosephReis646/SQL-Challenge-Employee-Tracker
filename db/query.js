5a97a034436a520d3a128003f7fa80474e2bbc15
joseph reis <josephreis646@gmail.com>
2023-03-14 11:30:36 -0400

    function to retrieve all departments from the department table:

--- db/query.js
+++ db/query.js
@@ -1,1 +1,8 @@
+// Import the connection.js to query.js 
 const connection = require('./connection');
+
+// function to retrieve all departments from the department table:
+function getAllDepartments() {
+    return connection.promise().query('SELECT * FROM department');
+  }
+  
