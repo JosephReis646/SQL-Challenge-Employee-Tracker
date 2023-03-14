724dc7b6a300ab9e577dbdfe2236bed8b77151d6
joseph reis <josephreis646@gmail.com>
2023-03-14 11:32:46 -0400

    function to retrieve all employees from the employee table, including their role, department, and manager

--- db/query.js
+++ db/query.js
@@ -14,3 +14,14 @@ function getAllRoles() {
     `);
   }
   
+//   function to retrieve all employees from the employee table, including their role, department, and manager:
+function getAllEmployees() {
+    return connection.promise().query(`
+      SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+      FROM employee
+      INNER JOIN role ON employee.role_id = role.id
+      INNER JOIN department ON role.department_id = department.id
+      LEFT JOIN employee AS manager ON employee.manager_id = manager.id
+    `);
+  }
+  
